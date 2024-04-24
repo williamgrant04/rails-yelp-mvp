ce62c278843e1953b8dc0ed57025b1aed78fcd99
William Grant <williamg3575467@gmail.com>
2024-04-24 11:36:29 +0100

    Pass all tests - implement crud methods for restaurants and reviews

---
+++ app/models/review.rb
@@ -0,0 +1,10 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :restaurant, presence: true
+
+  validates :content, presence: true
+
+  validates :rating, presence: true
+  validates :rating, numericality: { only_integer: true, in: (0..5) }
+end
